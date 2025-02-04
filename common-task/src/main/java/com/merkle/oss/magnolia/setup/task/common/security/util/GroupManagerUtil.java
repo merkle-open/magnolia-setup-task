221a6b8b4debb5902c35591c1cc18985ca5fff5b
eschleb <bjoern.eschle@merkle.com>
2025-02-04 15:43:53 +0100

    Implement GroupManagerUtil group modify methods

--- common-task/src/main/java/com/merkle/oss/magnolia/setup/task/common/security/util/GroupManagerUtil.java
+++ common-task/src/main/java/com/merkle/oss/magnolia/setup/task/common/security/util/GroupManagerUtil.java
@@ -1,24 +1,22 @@
 package com.merkle.oss.magnolia.setup.task.common.security.util;
 
-import info.magnolia.cms.security.AccessDeniedException;
 import info.magnolia.cms.security.Group;
 import info.magnolia.cms.security.GroupManager;
 import info.magnolia.cms.security.SecuritySupport;
 
-import java.lang.invoke.MethodHandles;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
+import javax.annotation.Nullable;
 import javax.inject.Inject;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.machinezoo.noexception.Exceptions;
 
 public class GroupManagerUtil {
-	private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 	private final Supplier<GroupManager> groupManager;
 
 	@Inject
@@ -34,11 +32,52 @@ public class GroupManagerUtil {
 	}
 
 	public Optional<Group> getGroup(final String groupName) {
-		try {
-			return Optional.ofNullable(groupManager.get().getGroup(groupName));
-		} catch (AccessDeniedException e) {
-			LOG.error("Access denied to get group " + groupName, e);
-			return Optional.empty();
+		return Optional.ofNullable(Exceptions.wrap().get(() -> groupManager.get().getGroup(groupName)));
+	}
+
+	public Group getOrCreateGroup(final String name) throws Exception {
+		@Nullable final Group group = getGroup(name).orElse(null);
+		if (group == null) {
+			return groupManager.get().createGroup(name);
 		}
+		return group;
+	}
+
+	public void setRoles(final Group group, final Set<String> roleNames) {
+		removeRoles(group, group.getRoles());
+		roleNames.forEach(roleName ->
+			addRole(group, roleName)
+		);
+	}
+	public void addRole(final Group group, final String roleName) {
+		Exceptions.wrap().run(() -> groupManager.get().addRole(group, roleName));
+	}
+
+	public void removeRoles(final Group group, final Collection<String> roleNames) {
+		roleNames.forEach(role ->
+				removeRole(group, role)
+		);
+	}
+	public void removeRole(final Group group, final String roleName) {
+		Exceptions.wrap().run(() -> groupManager.get().removeRole(group, roleName));
+	}
+
+	public void setGroups(final Group group, final Set<String> groupNames) {
+		removeRoles(group, group.getRoles());
+		groupNames.forEach(groupName ->
+				addGroup(group, groupName)
+		);
+	}
+	public void addGroup(final Group group, final String groupName) {
+		Exceptions.wrap().run(() -> groupManager.get().addGroup(group, groupName));
+	}
+
+	public void removeGroups(final Group group, final Collection<String> groupNames) {
+		groupNames.forEach(groupName ->
+				removeGroup(group, groupName)
+		);
+	}
+	public void removeGroup(final Group group, final String groupName) {
+		Exceptions.wrap().run(() -> groupManager.get().removeGroup(group, groupName));
 	}
 }
